db1b8b9dd7a1010dc423b6ea89fe861a6843d5a7
m.kozhukhov <neogenka@gmail.com>
2026-01-07 20:00:07 +0300

    schedule model fix

--- Tracker/Domain/Models/Models.swift
+++ Tracker/Domain/Models/Models.swift
@@ -13,6 +13,32 @@ enum WeekDay: Int, CaseIterable, Codable {
     case monday, tuesday, wednesday, thursday, friday, saturday, sunday
 }
 
+extension WeekDay {
+    var shortTitle: String {
+        switch self {
+        case .monday: return "Пн"
+        case .tuesday: return "Вт"
+        case .wednesday: return "Ср"
+        case .thursday: return "Чт"
+        case .friday: return "Пт"
+        case .saturday: return "Сб"
+        case .sunday: return "Вс"
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .monday: return "Понедельник"
+        case .tuesday: return "Вторник"
+        case .wednesday: return "Среда"
+        case .thursday: return "Четверг"
+        case .friday: return "Пятница"
+        case .saturday: return "Суббота"
+        case .sunday: return "Воскресенье"
+        }
+    }
+}
+
 struct TrackerCategory {
     let id: UUID
     let title: String
