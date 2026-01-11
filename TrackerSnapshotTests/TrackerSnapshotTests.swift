2f6de072ad077cb44964abd433d60d67d6b40cd0
m.kozhukhov <neogenka@gmail.com>
2026-01-11 18:12:39 +0300

    snapshot tests added

---
+++ TrackerSnapshotTests/TrackerSnapshotTests.swift
@@ -0,0 +1,12 @@
+//
+//  TrackerSnapshotTests.swift
+//  TrackerSnapshotTests
+//
+//  Created by МAK on 11.01.2026.
+//
+
+import Testing
+
+struct TrackerSnapshotTests {
+    @Test func example() async throws {}
+}
